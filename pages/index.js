d2c212b5510fc6fdd890c454443d2c4f14c11cac
General Orange <erik.17021995@gmail.com>
2021-03-12 21:26:57 +0100

    First few changes

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,35 @@
 import Head from 'next/head'
+import Image from 'next/image';
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const env = 'http://localhost:1337';
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+export default function Home({ homepage }) {
+  const imagePath = env+homepage.header.image.formats.large.url;
+  return (<>
+    <Head>
+      <title>{homepage.name}</title>
+    </Head>
+    <div className="container">
+      <div className={styles.header}>
+        {homepage.header.content}
+        <Image src={imagePath} width={homepage.header.image.formats.large.width} height={homepage.header.image.formats.large.height}></Image>
+      </div>
     </div>
-  )
+    
+  </>)
+}
+
+export async function getStaticProps() {
+  
+  //get homepage from our api
+  const res = await fetch(env+'/homepage');
+  
+  const homepage = await res.json();
+
+  //console.log(homepage);
+
+  return {
+    props: { homepage },
+  }
 }
