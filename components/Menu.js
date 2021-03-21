a90807f9653e8dc41ac026ed192b4b28deb6be49
Erik van der Schaaf <erik@fuga.media>
2021-03-21 22:51:51 +0100

    Some progress made at parents

--- components/Menu.js
+++ components/Menu.js
@@ -1,28 +1,83 @@
 import Link from 'next/link'
+import styles from '../styles/Menu.module.css'
 
-const Menu = ({ menu }) => {
-    if(!menu){
+const MenuSections = ({ data }) => {
+  if(!data){
+    return (
+      <>
+      </>
+    )
+  }
+
+  var output = [];
+
+  { data.menu_section.map((data, index) => {
+    output.push(
+      (
+        <>
+          <div key={index} className={styles.MenuItem}>
+            <div className={styles.MenuItem__name}>
+              {data.name}
+            </div>
+            { SingleMenuSection( { data } ) }
+          </div>
+        </>
+      )
+    )
+    
+  })}
+
+  return output;
+}
+
+const SingleMenuSection = ({ data }) => {
+  if(!data){
+    return (
+      <>
+      </>
+    )
+  }
+
+  return (
+    <>
+      <div className={styles.MenuSection}>
+        <h2>{data.name}</h2>
+        <div>
+          {data.content}
+        </div>
+        {data.pages.map((key, value) => {
+          <Link key={key} href={`./${value.slug}`}>{value.name}</Link>
+        })}
+      </div>
+    </>
+  )
+  
+}
+
+const Menu = ({ data }) => {
+    if(!data){
         return (
             <>
             </>
         )
     }
 
-    console.log(menu.menu_section);
+    return (
+      <>
+        <div className={styles.Menu__outer}>
+          <div className="container">
+            <div className="row">
+              <div className="col-12">
+                <div className={styles.Menu}>
+                  { MenuSections({ data }) }
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    )
     
-}
-
-export async function getStaticProps() {
-    //get homepage from our api
-  const resMenu = await fetch(process.env.API_ENDPOINT+'/menu');
-  
-  const menu = await resMenu.json();
-
-  return {
-    props: {
-      menu,
-    }
-  }
 }
 
 export default Menu;
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,12 @@ import Headings from '../processing/Headings'
 import HeaderBanner from '../components/HeaderBanner'
 import Menu from '../components/Menu'
 
-export default function Home({ homepage }) {
+export default function Home({ data }) {
 
   return (<>
-    <Headings data={homepage.meta_data} />
-    <Menu />
-    <HeaderBanner data={homepage.header} />
+    <Headings data={data.homepage.meta_data} />
+    <Menu data={ data.menu }/>
+    <HeaderBanner data={data.homepage.header} />
     
   </>)
 }
@@ -16,12 +16,19 @@ export async function getStaticProps() {
   
   //get homepage from our api
   const resHome = await fetch(process.env.API_ENDPOINT+'/homepage');
+  const resMenu = await fetch(process.env.API_ENDPOINT+'/menu');
   
   const homepage = await resHome.json();
+  const menu = await resMenu.json();
+
+  let data = {
+    homepage,
+    menu
+  }
 
   return {
     props: {
-      homepage,
+      data,
     }
   }
 }
