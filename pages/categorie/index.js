8ff054667e40fa5bb1fc0376382df580900cd7ea
General Orange <erik.17021995@gmail.com>
2021-04-16 00:57:27 +0200

    Fix on `Build` script

--- pages/categorie/index.js
+++ pages/categorie/index.js
@@ -1,1 +1,17 @@
-//gold
+
+const CategoryCollection = ({ data }) => {
+    if(!data){
+        return (
+            <>
+            </>
+        )
+    }
+
+    return (
+        <>
+            test123
+        </>
+    )
+}
+
+export default CategoryCollection;
