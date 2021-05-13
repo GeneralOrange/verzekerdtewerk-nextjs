e970d24e32ca6949faca752528d721f23231bf2d
General Orange <erik.17021995@gmail.com>
2021-05-14 00:46:07 +0200

    Testing webhook

--- pages/api/discord.js
+++ pages/api/discord.js
@@ -1,7 +1,15 @@
 module.exports = (req, res) => {
-    res.json({
-      body: req.body,
-      query: req.query,
-      cookies: req.cookies,
+    const webhook_url = 'https://discord.com/api/webhooks/842522800699080735/V4EPKnHNauwJgFoCnJD6FOcLCsDGjftZvjIZO0bd2-VIAIbb3kwMnYAmXfB6pQIZ3D1w?wait=true';
+
+    const msg = {
+        "content": "Dit is een test!"
+    }
+
+    fetch(webhook_url, {
+        "method": "POST",
+        "headers": {
+            "content-type": "application/json"
+        },
+        "body": JSON.stringify(msg)
     })
   }
