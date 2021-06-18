8efc31bb97de1b052be28dc7424aa0faf878fe73
General Orange <erik.17021995@gmail.com>
2021-06-18 21:04:19 +0200

    Desktop accordion fixes

--- components/flexcontent/AccordionComponentItem.js
+++ components/flexcontent/AccordionComponentItem.js
@@ -1,5 +1,6 @@
 import MarkDown from '../../processing/MarkDown'
 import { Card, Accordion } from 'react-bootstrap'
+import { FaHome } from 'react-icons/fa'
 import styles from '../../styles/flexcontent/AccordionComponent.module.scss'
 
 export default function AccordionComponentItem({ data, eventKey }){
@@ -11,7 +12,9 @@ export default function AccordionComponentItem({ data, eventKey }){
         <div className={ styles.AccordionComponent__body }>
             <Accordion.Collapse eventKey={ eventKey } className={ styles.AccordionComponent__bodyInner }>
                 <Card.Body>
+                    <div className={styles.AccordionComponent__step}>Stap: { eventKey }</div>
                     <MarkDown data={data.description}/>
+                    <FaHome className={styles.AccordionComponent__icon}/>
                 </Card.Body>
             </Accordion.Collapse>
         </div>
