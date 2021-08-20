a57720a092d4ef53738946aa1c91b8c05ef92a63
General Orange <erik.17021995@gmail.com>
2021-08-20 02:18:12 +0200

    Added some conditional logic to prevnext component

--- components/PrevNext.js
+++ components/PrevNext.js
@@ -85,15 +85,18 @@ export default function PrevNext({ data, pageType, nextValue }){
                     </Container>
                 )
             } else if(data.job){
+                const enableRouter = routerException(data.job.slug);
+                const prevLink = enableRouter ? `/werkzaamheden/` : `/werkzaamheden/${data.job.slug}/`;
+                const prevText = enableRouter ? `Terug naar werkzaamheden` : `Terug naar werkzaamheid`;
                 return (
                     <Container>
                         <Row>
                             <Col>
                                 <div className={ styles.PrevNext }>
                                     <div className={ styles.PrevNext__prev }>
-                                        <Link href={`/werkzaamheden/`}>
+                                        <Link href={ prevLink }>
                                             <a>
-                                                <FaChevronLeft/> Terug naar werkzaamheden
+                                                <FaChevronLeft/> { prevText }
                                             </a>
                                         </Link>
                                     </div>
