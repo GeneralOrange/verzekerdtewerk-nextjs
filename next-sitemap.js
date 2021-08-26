00b893d48b5161c77d6f94f3dbf672bb73e7d5c2
General Orange <erik.17021995@gmail.com>
2021-08-26 21:59:46 +0200

    Exclude certain paths from sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -2,4 +2,10 @@ module.exports = {
     siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://example.com',
     generateRobotsTxt: true, // (optional)
     sitemapSize: 7000, // ...other options
+    exclude: [
+        '/werkzaamheden/alarm-systeem-installatie-of-vervanging/kosten/',
+        '/werkzaamheden/airco-plaatsen-of-vervangen/kosten/',
+        '/werkzaamheden/dakgoot-schoonhouden/kosten/',
+        '/werkzaamheden/schoorsteen-vegen/kosten/'
+    ]
 }
