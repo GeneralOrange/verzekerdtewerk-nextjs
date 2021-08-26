0a4a4d03c95fb1f77a386c4882a5ca0db476db7c
General Orange <erik.17021995@gmail.com>
2021-08-26 22:08:45 +0200

    Sitemap fix attempt 2

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,11 +1,11 @@
 module.exports = {
     siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://example.com',
-    generateRobotsTxt: true, // (optional)
-    sitemapSize: 7000, // ...other options
+    generateRobotsTxt: true,
+    sitemapSize: 5000,
     exclude: [
-        '/werkzaamheden/alarm-systeem-installatie-of-vervanging/kosten/',
-        '/werkzaamheden/airco-plaatsen-of-vervangen/kosten/',
-        '/werkzaamheden/dakgoot-schoonhouden/kosten/',
-        '/werkzaamheden/schoorsteen-vegen/kosten/'
+        '/werkzaamheden/alarm-systeem-installatie-of-vervanging/kosten',
+        '/werkzaamheden/airco-plaatsen-of-vervangen/kosten',
+        '/werkzaamheden/dakgoot-schoonhouden/kosten',
+        '/werkzaamheden/schoorsteen-vegen/kosten'
     ]
 }
